e888cc57d9300f0bc483af65dbca355489bc85af
99choco99 <99choco99@naver.com>
2024-07-03 19:10:15 +0900

    240703
    애니메이션 및 무한맵 추가

--- Assets/Scenes/Script/Player.cs
+++ Assets/Scenes/Script/Player.cs
@@ -7,10 +7,14 @@ public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     public float speed;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -27,5 +31,14 @@ public class Player : MonoBehaviour
         //3. 위치 이동
         Vector2 nextVec = inputVec.normalized * speed *Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
